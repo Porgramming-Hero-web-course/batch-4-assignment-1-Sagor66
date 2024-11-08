46939357d05944210e7123357187a3263cbe2e57
sagor66 <sazzadulhaquesagor@gmail.com>
2024-11-08 20:39:11 +0600

    solved: problem 6

---
+++ problems/src/problem-6.ts
@@ -0,0 +1,12 @@
+interface Profile {
+  name: string;
+  age: number;
+  email: string;
+}
+
+const updateProfile = (
+  profile: Profile,
+  updatedValue: Partial<Profile>
+): Profile => {
+  return { ...profile, ...updatedValue };
+};
